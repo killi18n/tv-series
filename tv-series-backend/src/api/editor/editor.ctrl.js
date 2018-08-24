ef102bc8d8e014ed1649c364464a67b8b66e6de3
killi8n <killi8n@gmail.com>
2018-08-24 18:35:58 +0900

    bug fixed

--- tv-series-backend/src/api/editor/editor.ctrl.js
+++ tv-series-backend/src/api/editor/editor.ctrl.js
@@ -1,46 +1,48 @@
-const fs = require('fs');
-const path = require('path');
-const uploadDirPath = '../../../uploads';
+const fs = require("fs");
+const path = require("path");
+const uploadDirPath = "../../../uploads";
+const { decodeToken } = require("lib/token");
 
 exports.checkAdmin = async (ctx, next) => {
-    const token = ctx.cookies.get('access_token');
+  const token = ctx.cookies.get("access_token");
 
-    const decoded = await decodeToken(token);
+  const decoded = await decodeToken(token);
+  console.log(decoded);
+  const { admin } = decoded;
 
-    const { admin } = decoded;
+  if (!admin) {
+    ctx.status = 401;
+    return;
+  }
 
-    if (!admin) {
-        ctx.status = 401;
-        return;
-    }
+  return next();
+};
 
-    return next();
-
-}
-
-exports.uploadImage = async (ctx) => {
-    if(!ctx.request.body.files.image) {
-        ctx.status = 204;
-        return;
-    }
-    const file = ctx.request.body.files.image;
-    const storageFileName = Math.random().toString();
-    const reader = fs.createReadStream(file.path);
-    const stream = fs.createWriteStream(path.join(__dirname + uploadDirPath, storageFileName + ".jpg"));
-    reader.pipe(stream);
-    ctx.body = {
-        storedFileName: storageFileName + ".jpg",
-        localFileName: file.name
-    };
-}
-
-exports.deleteImage = async (ctx) => {
-    const {image} = ctx.request.body;
-    if(!fs.existsSync(path.join(__dirname + uploadDirPath, image))) {
-        ctx.status = 204;
-        return;
-    }
-    fs.unlinkSync(path.join(__dirname + uploadDirPath, image));
+exports.uploadImage = async ctx => {
+  if (!ctx.request.body.files.image) {
     ctx.status = 204;
     return;
-}
+  }
+  const file = ctx.request.body.files.image;
+  const storageFileName = Math.random().toString();
+  const reader = fs.createReadStream(file.path);
+  const stream = fs.createWriteStream(
+    path.join(__dirname + uploadDirPath, storageFileName + ".jpg")
+  );
+  reader.pipe(stream);
+  ctx.body = {
+    storedFileName: storageFileName + ".jpg",
+    localFileName: file.name
+  };
+};
+
+exports.deleteImage = async ctx => {
+  const { image } = ctx.request.body;
+  if (!fs.existsSync(path.join(__dirname + uploadDirPath, image))) {
+    ctx.status = 204;
+    return;
+  }
+  fs.unlinkSync(path.join(__dirname + uploadDirPath, image));
+  ctx.status = 204;
+  return;
+};
